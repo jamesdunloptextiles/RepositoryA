5653e7748d79409f157ccc0980b89581232ddb98
jamesdunloptextiles <rajanb@jamesdunlopgroup.com>
2023-03-10 11:09:42 +1300

    Test Sap content

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://jdg_sap101_test"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "SAP 101 at James Dunlop Group"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;strong&gt;Welcome to SAP 101&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;This course is designed to introduce you to SAP S/4HANA. It will provide you with the basics, so you become familiar with how to navigate around the system and learn some tips and tricks to make things easier and to provide you with a sneak peek into the look and feel of the system.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Let&amp;#39;s get started. Have fun!&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Hint:&amp;nbsp;&lt;/strong&gt;You can start by clicking on the title of the first lesson below.&lt;/p&gt;"
+};
